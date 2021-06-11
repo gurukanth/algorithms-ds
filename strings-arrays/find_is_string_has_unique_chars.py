f75bfd76baff69dbe926c1cd5f2eca56003ce374
Venu <venugrda@gmail.com>
2021-06-10 21:39:15 -0400

    implmented if string has uniq chars using brute force method

--- strings-arrays/find_is_string_has_unique_chars.py
+++ strings-arrays/find_is_string_has_unique_chars.py
@@ -1,4 +1,18 @@
 
 def has_unique_chars_brute_force(string:str):
-    for i in range(0, len(string)):
-        for
+    for i in range(len(string)):
+        for j in range(i + 1, len(string)):
+            if string[i] == string[j]:
+                return False
+    return True
+
+
+assert False == has_unique_chars_brute_force('HelloWorld')
+assert True == has_unique_chars_brute_force('abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+test_code = '''
+def test():
+    return   has_unique_chars_brute_force('abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+'''
+import timeit
+time = timeit.Timer(stmt=test_code).timeit()
+print(time)
